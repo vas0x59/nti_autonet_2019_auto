24113ccfff42bde375b61ab7115b5529c7b1dc56
Vas2016 <vasily.yuryev.a1@gmail.com>
2019-04-11 20:54:06 +0300

    Svet

--- RegSvet.py
+++ RegSvet.py
@@ -8,13 +8,58 @@ from tensorflow.keras.models import model_from_json
 import dlib
 
 class RegSvet:
-    def __init__():
-        pass
-    def load_model_nn(w, j):
+    def __init__(self, vs):
+        self.labels = ['red', 'yellow', 'green']
+        self.label = "none"
+        self.vs = vs
+        self.reg_t = False
+        self.stop_t = False
+    def load_model_nn(self, w, j):
+        f = open(j, 'r')
+        json_string = f.readline()
+        f.close()
+        self.model = model_from_json(json_string)
+        self.model.load_weights(w)
+    def load_model_svm(self, p):
+        self.model_detector = dlib.simple_object_detector("tld.svm")
 
-    def load_model_svm(p):
-
-    def predict_label(img):
-        return
+    def predict_label(self, image):
+        image = cv2.resize(image, (38, 38))
+        # image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+        image = img_to_array(image)
+        image = np.expand_dims(image, axis=0)
+        image = np.array(image, dtype="float") / 255.0
+        pre = self.model.predict(image)[0]
+        i_max = 0
+        for i in range(len(pre)):
+            if pre[i_max] < pre[i]:
+                i_max = i
+        return self.labels[i_max]
     
-    def reg_svet(img):
+    def reg_svet(self, img):
+        boxes = model_detector(frame)
+        label = "none"
+        for box in boxes:
+            print (box)
+            (x, y, xb, yb) = [box.left(), box.top(), box.right(), box.bottom()]
+            crop = frame[y:yb, x:xb].copy()
+            label = self.predict_label(crop)
+        return label
+
+    def reg_toggle(self, q):
+        self.reg_t = q
+    def stop(self):
+        self.stop_t = True
+    def update(self):
+        while not stop_t:
+            frame = self.vs.read()
+            if stop_t:
+                break
+            self.label = self.reg_svet()
+    def get_label(self):
+        return self.label
+
+    def init_reg(self):
+        t = Thread(target=self.update, args=())
+        t.daemon = True
+		t.start()
